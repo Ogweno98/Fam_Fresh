52f157e5288fc1961431bd345f470b37ae14d67e
jompodigitalsolutions <66160451+Ogweno98@users.noreply.github.com>
2025-11-15 14:07:29 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/FamFresh/',
+  base: '/Fam_Fresh/',
   plugins: [react()]
 });
